c3f68446c207e04ffd5c2b67ab2e47ca5379e74a
Oleksandr <alex_is_ok@yahoo.com>
2018-11-04 04:25:25 +0200

    code is refactored new test is added

--- tasks/251.rb
+++
@@ -1,9 +0,0 @@
-"
-дана строка символов; подсчитать, сколько раз среди
-символов строки встречается буква x
-"
-
-def task251(text: )
- count = text.split('').map(&:to_s).select { |item| item.include?("x") }
- count.length
-end
--- tasks/328.rb
+++
@@ -1,9 +0,0 @@
-"
-Найти 100 первых простых чисел.
-"
-
-require 'prime'
-
-def task328
-  Prime.first(100)
-end
